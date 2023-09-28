df3254d78dc35f0b5a74d828c6247e14fafc980d
alejo8420 <alejandro.salazar8420@gmail.com>
2023-09-28 15:33:40 -0500

    application.properties

--- src/main/java/com/platzi/pizza/PlatziPizzeriaApplication.java
+++ src/main/java/com/platzi/pizza/PlatziPizzeriaApplication.java
@@ -8,7 +8,6 @@ public class PlatziPizzeriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlatziPizzeriaApplication.class, args);
-		System.out.println("Hola mundo");
 	}
 
 }
