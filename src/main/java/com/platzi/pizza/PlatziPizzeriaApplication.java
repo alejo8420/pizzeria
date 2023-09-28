5c9f180a7928d9b878da4f971964943b75445e89
alejo8420 <alejandro.salazar8420@gmail.com>
2023-09-28 15:21:47 -0500

    commit prueba

--- src/main/java/com/platzi/pizza/PlatziPizzeriaApplication.java
+++ src/main/java/com/platzi/pizza/PlatziPizzeriaApplication.java
@@ -8,6 +8,7 @@ public class PlatziPizzeriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlatziPizzeriaApplication.class, args);
+		System.out.println("Hola mundo");
 	}
 
 }
